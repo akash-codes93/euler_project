72037fc9608c255846337bc58af02ad6e46f82d1
akash-github-projects <akash5gupta5@gmail.com>
2017-08-26 13:06:58 +0530

    Second question

---
+++ 2.py
@@ -0,0 +1,9 @@
+from generator import febonacci
+
+
+def euler_2(n):
+    return sum([i for i in febonacci(1,2,4000000) if i%2==0])
+
+
+if __name__ == '__main__':
+    print(euler_2(4000000))
--- generator.py
+++ generator.py
@@ -3,3 +3,14 @@ def natural(start, end):
     while i< end:
         yield i
         i = i+1
+
+
+def febonacci(a,b,limit):
+    c = a+b
+    yield a
+    yield b
+    while (a+b) < limit:
+        c = a+b
+        yield c
+        a = b
+        b = c
